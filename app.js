8f0b1abea7acd457f42fecd4544b943a6f5e3e76
Pavka <d.pavka@gmail.com>
2017-12-19 18:16:55 +0200

    log text changed

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-var pmx     = require('pmx');
-var config    = pmx.initModule();
+var pmx = require('pmx');
+var config = pmx.initModule();
 
-var pm2       = require('pm2');
+var pm2 = require('pm2');
 var SysLogger = require('ain2');
-var logger    = new SysLogger({tag: 'pm2',  facility: config.facility});
+var logger = new SysLogger({tag: 'pm2', facility: config.facility});
 
-pm2.launchBus(function(err, bus) {
-  bus.on('*', function(event, data){
-    if (event == 'process:event') {
-      logger.warn("===========" +'ng-e.id-api event ======= restart_count=%s status=%s',
-                  data.process.restart_time,
-                  data.event);
-    }
-  });
+var indent = ' '.repeat(20);
 
-  bus.on('log:err', function(data) {
-    logger.error("===========" + 'ERROR ng-e.id-api id=%s ==>%s', data.process.pm_id, data.data);
-  });
+pm2.launchBus(function (err, bus) {
+    bus.on('*', function (event, data) {
+        if (event == 'process:event') {
+            logger.warn(indent + 'EVENT %s', data.event);
+        }
+    });
 
-  bus.on('log:out', function(data) {
-    logger.log("===========" +'ng-e.id-api id=%s    %s', data.process.pm_id, data.data);
-  });
+    bus.on('log:err', function (data) {
+        logger.error(indent + 'ERROR %s', data.data);
+    });
+
+    bus.on('log:out', function (data) {
+        logger.log(indent + 'MESSAGE %s', data.data);
+    });
 });

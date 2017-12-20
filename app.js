abbcd89f1aa7d9e5d6ebeea048b5c615da52e8ba
Pavka <d.pavka@gmail.com>
2017-12-20 11:52:28 +0200

    log text changed

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ var pm2 = require('pm2');
 var SysLogger = require('ain2');
 var logger = new SysLogger({tag: 'pm2', facility: config.facility});
 
-var indent = ' '.repeat(20);
+var prefix = 'E.ID > ';
 
 pm2.launchBus(function (err, bus) {
     bus.on('*', function (event, data) {
         if (event == 'process:event') {
-            logger.warn(indent + 'EVENT %s', data.event);
+            logger.warn(prefix + 'EVENT %s', data.event);
         }
     });
 
     bus.on('log:err', function (data) {
-        logger.error(indent + 'ERROR %s', data.data);
+        logger.error(prefix + 'ERROR %s', data.data);
     });
 
     bus.on('log:out', function (data) {
-        logger.log(indent + 'MESSAGE %s', data.data);
+        logger.log(prefix + 'MESSAGE %s', data.data);
     });
 });
